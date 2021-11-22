df77ed1c65762142bb46f26ed929c3582c5aa687
Ryan Hedges <ryanhedges@gmail.com>
2021-11-22 14:54:25 -0800

    Add sourcing of zprofile for brew to work during install
    I did this because brew needs to be in the path to function properly.
    After the echo and eval, the terminal doesn't have that updated path
    info for future commands.
    
    ps-id: 75D4746C-28E3-40C5-A598-D1EE283C3A4D

--- settop.sh
+++ settop.sh
@@ -42,6 +42,7 @@ if ! command -v brew >/dev/null; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
   echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
   eval "$(/opt/homebrew/bin/brew shellenv)"
+  source ~/.zprofile
 else
   yel_print "Homebrew already installed"
 fi
